34ab166a61100019ed9e85169826689f27fb1c67
Andrecatt156 <af245@njit.edu>
2016-04-01 13:37:14 -0400

    Added files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -88,13 +88,13 @@ var mJson;
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
 
-if ($_GET["json"] === "extra.json")
+if ($_GET["json"] === "images.json")
 {
-    var mUrl = "extra.json";
+    var mUrl = "images.json";
 } 
 else 
 {
-    var mUrl = "images.json";
+    mUrl = "extra.json";
 }
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
